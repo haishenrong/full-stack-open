22d6afded76472875d41b9acc15ce26e6f8cbba7
Lyrenyx Maxima <haishenrong@gmail.com>
2021-05-15 10:49:49 -0400

    Part 2e

--- part2/src/App.js
+++ part2/src/App.js
@@ -3,12 +3,38 @@ import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
 
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )
+}
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2021</em>
+    </div>
+  )
+}
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('') 
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+
   
   useEffect(() => {
     noteService
@@ -38,7 +64,6 @@ const App = () => {
   }
 
   const handleNoteChange = (event) => {
-    //console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
@@ -56,9 +81,12 @@ const App = () => {
       setNotes(notes.map(note => note.id !== id ? note : shownNotes))
     })
     .catch(error => {
-      alert(
-        `the note '${note.content}' was already deleted from server`
+      setErrorMessage(
+        `Note '${note.content}' was already removed from server`
       )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
       setNotes(notes.filter(n => n.id !== id))
     })
   }
@@ -66,6 +94,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
@@ -85,7 +114,8 @@ const App = () => {
           onChange={handleNoteChange} 
         />
         <button type="submit">save</button>
-      </form>   
+      </form> 
+      <Footer />  
     </div>
   )
 }
