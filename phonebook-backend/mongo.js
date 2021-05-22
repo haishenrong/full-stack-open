9590345acb84bab36fab872ce36f4700193e5b2a
Lyrenyx Maxima <haishenrong@gmail.com>
2021-05-22 11:42:04 -0400

    Exercise 3.12

---
+++ phonebook-backend/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length === 4 || process.argv.length < 3) {
+  console.log('Please provide the password as an argument for displaying entries: node mongo.js <password>')
+  console.log('Please provide the name and number of the person you are adding: node mongo.js <password> <name> <person>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.br905.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+ personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv.length === 3) {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
+}
+
+if(process.argv.length === 5) {
+  const person = new Person({
+    name: name,
+    number: number
+  })
+
+  person.save().then(result => {
+    console.log('person saved!')
+    mongoose.connection.close()
+  })
+}
+
