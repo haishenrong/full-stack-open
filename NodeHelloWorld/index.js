f954942b87c6d9bb4a6c6914bffdc586ef2ee340
Lyrenyx Maxima <haishenrong@gmail.com>
2021-05-23 16:22:11 -0400

    Part 3d added DB to production build + ESlint

--- NodeHelloWorld/index.js
+++ NodeHelloWorld/index.js
@@ -87,8 +87,7 @@ app.post('/api/notes', (request, response, next) => {
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
+    .then(result => {response.status(204).end()
     })
     .catch(error => next(error))
 })
