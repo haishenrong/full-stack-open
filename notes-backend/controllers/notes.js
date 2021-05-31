174d6c8a98a31c46d94a29872f761dad83db5a37
Lyrenyx Maxima <haishenrong@gmail.com>
2021-05-31 12:16:57 -0400

    Part 4c User Administration

--- notes-backend/controllers/notes.js
+++ notes-backend/controllers/notes.js
@@ -1,8 +1,10 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note
+    .find({}).populate('user',{ username: 1, name: 1 })
   response.json(notes)
 })
 
@@ -18,6 +20,8 @@ notesRouter.get('/:id', async (request, response, next) => {
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   if (body.content === undefined) {
     return response.status(400).json({
       error: 'content missing'
@@ -27,9 +31,13 @@ notesRouter.post('/', async (request, response, next) => {
   const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date()
+    date: new Date(),
+    user: user._id
   })
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   response.json(savedNote)
 
 })
