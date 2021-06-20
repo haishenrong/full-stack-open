2ddd1414ed57c5d7d0ef9c75b3afef6242de591c
Lyrenyx Maxima <haishenrong@gmail.com>
2021-06-20 19:12:13 -0400

    Part 6c adding backend server + refactoring for redux-thunk

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Notes from './components/Notes'
 import NewNote from  './components/NewNote'
 import VisibilityFilter from './components/VisibilityFilter'
+import { initializeNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(initializeNotes())
+  }, [dispatch])
+
   return (
     <div>
       <NewNote />
