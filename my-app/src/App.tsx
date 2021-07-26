551801a2ba88638bbe12dbe73c8015a6ac025846
Lyrenyx Maxima <haishenrong@gmail.com>
2021-07-25 20:07:08 -0400

    Exercise 9.15 specifying course types

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,25 +4,77 @@ import Content from './components/Content';
 import Total from './components/Total';
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  // new types
+  interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+    type: string;
+  }
+
+  interface CoursePartDescription extends CoursePartBase {
+    description: string;
+  }
+
+  interface CourseNormalPart extends CoursePartDescription {
+    type: "normal";
+  }
+  interface CourseProjectPart extends CoursePartBase {
+    type: "groupProject";
+    groupProjectCount: number;
+  }
+
+  interface CourseSubmissionPart extends CoursePartDescription {
+    type: "submission";
+    exerciseSubmissionLink: string;
+  }
+
+  interface CourseSpecialPart extends CoursePartDescription {
+    type: "special";
+    requirements: string[];
+  }
+
+  type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart | CourseSpecialPart;
+
+  // this is the new coursePart variable
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is the leisured course part",
+      type: "normal"
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the harded course part",
+      type: "normal"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission"
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      type: "special"
     }
-  ];
+  ]
 
   return (
     <div>
       <Header name={courseName} />
-      <Content parts={courseParts} />
+      <Content parts={courseParts}/>
       <Total parts={courseParts} />
     </div>
   );
--- my-app/src/components/Content.tsx
+++ my-app/src/components/Content.tsx
@@ -1,25 +1,103 @@
 import React from 'react';
 
-interface CoursePart {
-  name: string;
-  exerciseCount: number;
-}
-interface ContentProps {
+interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+    type: string;
+  }
+
+  interface CoursePartDescription extends CoursePartBase {
+    description: string;
+  }
+
+  interface CourseNormalPart extends CoursePartDescription {
+    type: "normal";
+  }
+  interface CourseProjectPart extends CoursePartBase {
+    type: "groupProject";
+    groupProjectCount: number;
+  }
+
+  interface CourseSubmissionPart extends CoursePartDescription {
+    type: "submission";
+    exerciseSubmissionLink: string;
+  }
+
+  interface CourseSpecialPart extends CoursePartDescription {
+    type: "special";
+    requirements: string[];
+  }
+
+  type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart | CourseSpecialPart;
+
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
+  };
+  
+  const Part = (part:CoursePart) => {
+    switch (part.type) {
+      case "normal":
+        return (
+          <div>
+            <h2>
+              {part.name} {part.exerciseCount}
+            </h2>
+            <p>
+              {part.description}
+            </p>
+          </div>
+        );
+      case "groupProject":
+        return (
+          <div>
+            <h2>
+              {part.name} {part.exerciseCount}
+            </h2>
+            <p>
+              project exercises {part.groupProjectCount}
+            </p>
+          </div>
+        );
+      case "submission":
+        return (
+          <div>
+            <h2>
+              {part.name} {part.exerciseCount}
+            </h2>
+            <p>
+              {part.description}
+            <br></br>
+              submit to {part.exerciseSubmissionLink}
+            </p>
+          </div>
+        );
+      case "special":
+        return (
+          <div>
+            <h2>
+              {part.name} {part.exerciseCount}
+            </h2>
+            <p>
+              {part.description}
+            <br></br>
+              required skills: {part.requirements.join(', ')}
+            </p>
+          </div>
+        );
+      default:
+        return assertNever(part);
+    }
+  }
+  interface ContentProps {
     parts:CoursePart[]
 }
 
 const Content = (props:ContentProps) => {
-  return(
+  return (
     <div>
-      <p>
-        {props.parts[0].name} {props.parts[0].exerciseCount}
-      </p>
-      <p>
-        {props.parts[1].name} {props.parts[1].exerciseCount}
-      </p>
-      <p>
-        {props.parts[2].name} {props.parts[2].exerciseCount}
-      </p>
+      {props.parts.map(p => Part(p))}
     </div>
   )
 }
